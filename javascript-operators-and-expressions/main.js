60246f3351ef22971b076918d436558914700cd7
m-Cho1 <mturtle051@gmail.com>
2022-11-05 03:34:54 +0000

    completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+const width = 8;
+const height = 12;
+const area = width * height;
+console.log(`value of area: ${area}`);
+console.log(`typeof area: ${typeof (area)}`);
+
+const bill = 89;
+const payment = 120;
+const change = payment - bill;
+console.log(`value of change: ${change}`);
+console.log(`typeof change: ${typeof (change)}`);
+
+const quizzes = 20;
+const midterm = 89;
+const final = 97;
+const grade = (quizzes + midterm + final) / 3;
+console.log(`value of grade: ${grade}`);
+console.log(`typeof grade: ${typeof (grade)}`);
+
+const firstName = 'Minji';
+const lastName = 'Cho';
+const fullName = firstName + ' ' + lastName;
+console.log(`value of fullName: ${fullName}`);
+console.log(`typeof fullName: ${typeof (fullName)}`);
+
+const pH = 4;
+const isAcidic = pH < 7;
+console.log(`value of isAcidic: ${isAcidic}`);
+console.log(`typeof isAcidic: ${typeof (isAcidic)}`);
+
+const headCount = 8;
+const isSparta = headCount === 300;
+console.log(`value of isSparta: ${isSparta}`);
+console.log(`typeof isSparta: ${typeof (isSparta)}`);
+
+let motto = fullName;
+motto = fullName + ' is the GOAT';
+console.log(`value of motto: ${motto}`);
+console.log(`typeof motto: ${typeof (motto)}`);
