bdd75d51a8d42da85332bba85dca6c8b66fd665c
m-Cho1 <mturtle051@gmail.com>
2022-11-11 10:17:38 +0000

    fixed filter-out-strings.js to filter out strings

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -2,7 +2,7 @@
 function filterOutStrings(values) {
   const newArr = [];
   for (let i = 0; i < values.length; i++) {
-    if (typeof (values[i]) === 'number') {
+    if (typeof (values[i]) !== 'string') {
       newArr.push(values[i]);
     }
   }
